593585bf8dcd22a54f5149665e7107c9c8122484
sarang <saranggp2018@gmail.com>
2025-03-27 17:25:23 +0530

    corrected some erros

--- recipe-generate/app/page.tsx
+++ recipe-generate/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import { RecipeGenerator } from "./component/RecipeGenerator";
 
 export default function Home() {
   return (
    <div>
     <h1 className="bg-green-400 text-white tex-3xl font-bold">Recipe Generator </h1>
 
-  
+    <RecipeGenerator />
    </div>
   );
 }
