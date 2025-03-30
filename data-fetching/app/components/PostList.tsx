4e4d4f58d0cf329f3e428b0620cc78c3c761d042
sarang <saranggp2018@gmail.com>
2025-03-30 13:30:46 +0530

    completed postlist client page

---
+++ data-fetching/app/components/PostList.tsx
@@ -0,0 +1,29 @@
+
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+  }
+
+  interface PostListProps {
+    posts: Post[];
+  }
+  
+
+  const PostList : React.FC<PostListProps> = ({posts}) =>{
+    return(
+        <div className="space-y-4">
+            {
+                posts.map((post) =>{
+                    return(
+                        <div key={post.id} className="p-4 white rounded-lg shadow-md">
+                        <h3 className="text-xl font-bold">{post.title}</h3>
+                        <p className="mt-2 text-gray-600">{post.body}</p>
+                    </div>
+                    )
+                })
+            }
+        </div>
+    )
+  }
